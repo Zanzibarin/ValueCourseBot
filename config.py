7d759ddf1352ca8f3f8b7f0c1c503c5511790010
Zanzibarin <alex_potemkin@mail.ru>
2024-04-22 14:39:33 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = '6321289478:AAHFf0fr4-kSP7hKTGm1ZFQJSgpNOCLAtR0'
+
+keys = {'рубль': 'RU', 'доллар': 'US', 'евро': 'EU', 'юань': 'CHY'}
---
+++ extensions.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from config import keys
+
+
+class ConversionExeption(Exception):
+    pass
+
+
+class ValueConverter():
+    @staticmethod
+    # def convert(self, message: telebot.types.Message):
+    def convert(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise ConversionExeption(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConversionExeption(f'Не удалось обработать валюту "{quote}".')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConversionExeption(f'Не удалось обработать валюту "{base}".')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConversionExeption(f'Не удалось обработать количество "{amount}".')
+
+        # r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/2ceb7ceacfbb9494d7caed69/pair/{quote_ticker}/{base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
