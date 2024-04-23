0c19994bded05b1b687a77334b64c6cbbd02f968
Zanzibarin <alex_potemkin@mail.ru>
2024-04-23 19:06:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = '6321289478:AAHFf0fr4-kSP7hKTGm1ZFQJSgpNOCLAtR0'
 
-keys = {'рубль': 'RU', 'доллар': 'US', 'евро': 'EU', 'юань': 'CHY'}
+keys = {'рубль': 'RUB', 'доллар': 'USD', 'евро': 'EUR', 'юань': 'CNY'}
